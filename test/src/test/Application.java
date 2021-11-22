75cc199f495ff6e2fd68d05304dff1da1642cc88
danijela <dacafaca@gmail.com>
2021-11-22 14:01:55 +0100

    titled boarder added

--- test/src/test/Application.java
+++ test/src/test/Application.java
@@ -6,10 +6,31 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.ComponentOrientation;
+import java.awt.Component;
+import javax.swing.JMenu;
 
 public class Application {
 
 	private JFrame frame;
+	private JTextField textTextfieldUsername;
+	private JTextField textFieldPassword;
 
 	/**
 	 * Launch the application.
@@ -43,13 +64,55 @@ public class Application {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Login screen", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] {3};
-		gbl_panel.rowHeights = new int[] {5};
-		gbl_panel.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] {0, 0, 3};
+		gbl_panel.rowHeights = new int[] {0, 0, 0, 5};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
+		
+		JLabel lblUsername = new JLabel("Username");
+		GridBagConstraints gbc_lblUsername = new GridBagConstraints();
+		gbc_lblUsername.insets = new Insets(5, 0, 5, 5);
+		gbc_lblUsername.anchor = GridBagConstraints.EAST;
+		gbc_lblUsername.gridx = 0;
+		gbc_lblUsername.gridy = 0;
+		panel.add(lblUsername, gbc_lblUsername);
+		
+		textTextfieldUsername = new JTextField();
+		GridBagConstraints gbc_txtTextfieldUsername = new GridBagConstraints();
+		gbc_txtTextfieldUsername.insets = new Insets(5, 0, 5, 0);
+		gbc_txtTextfieldUsername.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtTextfieldUsername.gridx = 1;
+		gbc_txtTextfieldUsername.gridy = 0;
+		panel.add(textTextfieldUsername, gbc_txtTextfieldUsername);
+		textTextfieldUsername.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password");
+		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+		gbc_lblPassword.anchor = GridBagConstraints.EAST;
+		gbc_lblPassword.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPassword.gridx = 0;
+		gbc_lblPassword.gridy = 1;
+		panel.add(lblPassword, gbc_lblPassword);
+		
+		textFieldPassword = new JTextField();
+		GridBagConstraints gbc_textFieldPassword = new GridBagConstraints();
+		gbc_textFieldPassword.insets = new Insets(0, 0, 5, 0);
+		gbc_textFieldPassword.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldPassword.gridx = 1;
+		gbc_textFieldPassword.gridy = 1;
+		panel.add(textFieldPassword, gbc_textFieldPassword);
+		textFieldPassword.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		GridBagConstraints gbc_btnLogin = new GridBagConstraints();
+		gbc_btnLogin.gridx = 1;
+		gbc_btnLogin.gridy = 2;
+		panel.add(btnLogin, gbc_btnLogin);
+		
+		
 	}
-
 }
